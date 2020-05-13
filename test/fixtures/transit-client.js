a87e5a23ebc171119a82983a6ceb6aac3de5ae59
hyj1991 <yeekwanvong@gmail.com>
2020-05-14 00:01:22 +0800

    src: implement heartbeat

--- test/fixtures/transit-client.js
+++ test/fixtures/transit-client.js
@@ -4,7 +4,10 @@ const xtransit = require('../../xtransit');
 
 xtransit.start({
   server: process.env.UNIT_TEST_TRANSIT_CLIENT_SERVER,
-  reconnectBaseTime: process.env.UNIT_TEST_TRANSIT_CLIENT_RECONNECT_TIME
+  appId: process.env.UNIT_TEST_TRANSIT_APP_ID,
+  appSecret: process.env.UNIT_TEST_TRANSIT_APP_SECRET,
+  reconnectBaseTime: process.env.UNIT_TEST_TRANSIT_CLIENT_RECONNECT_TIME,
+  heartbeatInterval: process.env.UNIT_TEST_TRANSIT_HEARTBEAT_INTERVAL,
 });
 
 // expired time
