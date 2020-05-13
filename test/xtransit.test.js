86b580f5554eba8c3663452bf6d1aa20fdb7ea2c
hyj1991 <yeekwanvong@gmail.com>
2020-05-14 00:25:23 +0800

    src: implement heartbeat (#3)
    PR-URL: https://github.com/hyj1991/xtransit/pull/3
    Reviewed-BY: hyj1991 <yeekwanvong@gmail.com>

--- test/xtransit.test.js
+++ test/xtransit.test.js
@@ -7,8 +7,8 @@ const xtransit = require('../xtransit');
 const transitServer = path.join(__dirname, 'fixtures/transit-server.js');
 const transitClient = path.join(__dirname, 'fixtures/transit-client.js');
 
-describe('running xtransit', function () {
-  it('should throw if server is null', function () {
+describe('running xtransit', function() {
+  it('should throw if config.server is null', function() {
     let error = null;
     try {
       xtransit.start({ server: null });
@@ -17,9 +17,29 @@ describe('running xtransit', function () {
     }
     expect(error).to.be('config.server must be passed in!');
   });
+
+  it('should throw if config.appId is null', function() {
+    let error = null;
+    try {
+      xtransit.start({ server: 'ws://127.0.0.1:9090' });
+    } catch (err) {
+      error = err.message;
+    }
+    expect(error).to.be('config.appId must be passed in!');
+  });
+
+  it('should throw if config.appSecret is null', function() {
+    let error = null;
+    try {
+      xtransit.start({ server: 'ws://127.0.0.1:9090', appId: 5 });
+    } catch (err) {
+      error = err.message;
+    }
+    expect(error).to.be('config.appSecret must be passed in!');
+  });
 });
 
-describe('running xtransit', function () {
+describe('running xtransit', function() {
   const port = 9091;
   let xserver = null;
   let xclient = null;
@@ -30,8 +50,8 @@ describe('running xtransit', function () {
     xserver = cp.fork(transitServer, {
       env: Object.assign({}, process.env, {
         UNIT_TEST_TRANSIT_SERVER_PORT: port,
-        UNIT_TEST_TRANSIT_SERVER_RUNNING_TIME: 15000
-      })
+        UNIT_TEST_TRANSIT_SERVER_RUNNING_TIME: 15000,
+      }),
     });
     // wait for server opened
     await new Promise(resolve => xserver.on('message', msg => {
@@ -40,17 +60,17 @@ describe('running xtransit', function () {
       } else {
         const { type, data } = msg;
         switch (type) {
-        case 'new_client_count':
-          clientCount = data.clientCount;
-          break;
-        default:
-          console.log(`unknown ${type}: ${JSON.stringify(data)}`);
+          case 'new_client_count':
+            clientCount = data.clientCount;
+            break;
+          default:
+            console.log(`unknown ${type}: ${JSON.stringify(data)}`);
         }
       }
     }));
   }
 
-  before(async function () {
+  before(async function() {
     // start xserver
     await startServer();
 
@@ -58,11 +78,14 @@ describe('running xtransit', function () {
     xclient = cp.fork(transitClient, {
       env: Object.assign({}, process.env, {
         UNIT_TEST_TRANSIT_CLIENT_SERVER: `ws://127.0.0.1:${port}`,
+        UNIT_TEST_TRANSIT_APP_ID: 1,
+        UNIT_TEST_TRANSIT_APP_SECRET: 'mock',
+        UNIT_TEST_TRANSIT_CLIENT_RECONNECT_TIME: 1,
+        UNIT_TEST_TRANSIT_HEARTBEAT_INTERVAL: 1,
         UNIT_TEST_TRANSIT_CLIENT_RUNNING_TIME: 15000,
-        UNIT_TEST_TRANSIT_CLIENT_RECONNECT_TIME: 1
-      })
+      }),
     });
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise(resolve => setTimeout(resolve, 5000));
   });
 
   after(() => {
@@ -72,11 +95,11 @@ describe('running xtransit', function () {
     xclient = null;
   });
 
-  it('should run as expected.', function () {
+  it('should run as expected.', function() {
     expect(clientCount).to.be(1);
   });
 
-  it('should reconnected as expected.', async function () {
+  it('should reconnected as expected.', async function() {
     xserver.send('close');
     await new Promise(resolve => setTimeout(resolve, 3000));
     await startServer();
@@ -84,12 +107,14 @@ describe('running xtransit', function () {
     expect(clientCount).to.be(1);
   });
 
-  it('should connect failed with wss', async function () {
+  it('should connect failed with wss', async function() {
     const xclient2 = cp.fork(transitClient, {
       env: Object.assign({}, process.env, {
         UNIT_TEST_TRANSIT_CLIENT_SERVER: `wss://localhost:${port}`,
-        UNIT_TEST_TRANSIT_CLIENT_RUNNING_TIME: 15000
-      })
+        UNIT_TEST_TRANSIT_APP_ID: 1,
+        UNIT_TEST_TRANSIT_APP_SECRET: 'mock',
+        UNIT_TEST_TRANSIT_CLIENT_RUNNING_TIME: 15000,
+      }),
     });
     await new Promise(resolve => setTimeout(resolve, 500));
     expect(clientCount).to.be(1);
