a93bc88c8708c6cdfa99bb22c48ba91911ee9c42
hyj1991 <yeekwanvong@gmail.com>
2020-06-15 10:13:49 +0800

    refactor: config

--- xtransit.js
+++ xtransit.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const configure = require('./config');
 const XtransitAgent = require('./lib/agent');
 
-function start(userConfig) {
-  const defaultConfig = configure();
-  const config = Object.assign({}, defaultConfig, userConfig);
+function start(config) {
   const xtransitAgent = new XtransitAgent(config);
   xtransitAgent.run();
 }
