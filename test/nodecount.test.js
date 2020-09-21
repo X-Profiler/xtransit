66f7f2cafe70590cfb56407e641f64599934c027
hyj1991 <yeekwanvong@gmail.com>
2020-09-21 19:41:21 +0800

    style: lint

--- test/nodecount.test.js
+++ test/nodecount.test.js
@@ -3,12 +3,13 @@
 const path = require('path');
 const expect = require('expect.js');
 const {
-  getNodeCount
+  getNodeCount,
 } = require('../orders/system_log');
 
-describe('get node count correct', function () {
-  it('should run ok', async function () {
-    let count, err;
+describe('get node count correct', function() {
+  it('should run ok', async function() {
+    let count,
+      err;
     try {
       count = await getNodeCount();
     } catch (e) {
@@ -18,8 +19,9 @@ describe('get node count correct', function () {
     expect(count).to.be.ok();
   });
 
-  it('should run ok with space', async function () {
-    let count, err;
+  it('should run ok with space', async function() {
+    let count,
+      err;
     try {
       count = await getNodeCount(path.join(__dirname, 'fixtures/with space/get_node_processes.js'));
     } catch (e) {
