9b264f601a3f1f90de5d7701d089f62da86ce267
hyj1991 <yeekwanvong@gmail.com>
2022-10-07 13:38:41 +0800

    feat: set the maximum readable size

--- test/error.test.js
+++ test/error.test.js
@@ -5,6 +5,8 @@ const path = require('path');
 const expect = require('expect.js');
 const ErrorParser = require('../common/error');
 const errorFilePath = path.join(__dirname, './fixtures/files/error');
+const { map, parsers, MAX_READABLE_SIZE, MAX_ERROR_COUNT, readFileAsStream } = require('../orders/error_log');
+const bigError = fs.readFileSync(path.join(__dirname, './fixtures/files/big-error'), 'utf-8');
 
 describe('parse error logs', function() {
   const parser = new ErrorParser(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/i, 5);
@@ -36,5 +38,57 @@ describe('parse error logs', function() {
   it('should have 9 error logs', function() {
     expect(logs.length).to.be.ok();
     expect(logs.length).to.be(12);
+  });
+});
+
+describe('parse error logs', function() {
+  let start = 0;
+  let logs = [];
+  const tmpErrorPath = path.join(__dirname, './fixtures/tmp-error');
+
+  before(async function() {
+    fs.writeFileSync(tmpErrorPath, bigError);
+    parsers.set(tmpErrorPath, new ErrorParser(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/i, MAX_ERROR_COUNT));
+    logs = await readFileAsStream(tmpErrorPath, start);
+  });
+
+  after(function() {
+    fs.unlinkSync(tmpErrorPath);
+    map.delete(tmpErrorPath);
+    parsers.delete(tmpErrorPath);
+  });
+
+  it('should have error logs', async function() {
+    expect(logs.length).to.be(1);
+
+    // add 10 big log
+    start = map.get(tmpErrorPath);
+    for (let i = 0; i < 10; i++) {
+      fs.appendFileSync(tmpErrorPath, bigError);
+    }
+    logs = await readFileAsStream(tmpErrorPath, start);
+    expect(logs.length).to.be(10);
+
+    // add 10000 big log
+    const count = 10000;
+    console.time(`append ${count} big error log`);
+    start = map.get(tmpErrorPath);
+    for (let i = 0; i < count; i++) {
+      fs.appendFileSync(tmpErrorPath, bigError);
+    }
+    console.timeEnd(`append ${count} big error log`);
+    console.time(`parse ${count} big error log`);
+    logs = await readFileAsStream(tmpErrorPath, start);
+    console.log(`new error log size: ${bigError.length * 100}, readable limit: ${MAX_READABLE_SIZE} (error log ${bigError.length * 100 > MAX_READABLE_SIZE ? '>' : '<'} readable limit)`);
+    expect(logs.length).to.be(parseInt(MAX_READABLE_SIZE / bigError.length));
+    console.timeEnd(`parse ${count} big error log`);
+
+    // add 10 big log
+    start = map.get(tmpErrorPath);
+    for (let i = 0; i < 10; i++) {
+      fs.appendFileSync(tmpErrorPath, bigError);
+    }
+    logs = await readFileAsStream(tmpErrorPath, start);
+    expect(logs.length).to.be(10);
   });
 });
